d0a9f85a54309ca4e34f34094bcd7bb87cb20cd7
Ilgiz <ilgizbabanov@gmail.com>
2023-03-15 12:38:27 -0500

    initial commit

---
+++ src/test/java/tests/Phone.java
@@ -0,0 +1,10 @@
+package tests;
+
+import utilities.Driver;
+
+public class Phone {
+
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://www.google.com/");
+    }
+}
